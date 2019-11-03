c687df0ebb731fd589e4c1eadd1a12c6eab7980a
Mary Zeng <Mary.Zeng@Marys-MBP-2.wv.cc.cmu.edu>
2019-11-02 20:26:48 -0400

    startingmodes

--- main.py
+++ main.py
@@ -15,8 +15,31 @@ class Recipients(Users):
     def __init__():
         super().__init__()
         self
+
+class StartMode(Mode):
+    def redrawAll(mode, canvas):
+        font = 'Arial 26 bold'
+        canvas.create_rectangle(0, 0, mode.width, mode.height,
+                                fill = 'lightblue')
+        canvas.create_text(mode.width/2, 150, text='Are you a', font=font)
     
-    
+    ### selection: volunteers, Recipients
+
+    def mousePressed(mode, event):
+        if:
+            mode.app.setActiveMode(mode.app.VolunteersMode)
+        elif:
+            mode.app.setActiveMode(mode.app.RecipientsMode)
+
+
+class MyModalApp(ModalApp):
+    def appStarted(app):
+        app.StartMode = StartMode()
+        app.VolunteersMode = VolunteersMode()
+        app.RecipientsMode = RecipientsMode()
+
+        app.setActiveMode(app.StartMode)
+        app.timerDelay = 50
     
     
 ########
