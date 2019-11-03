e2e738fdd4defd5189e68c0aea551cf4f03e6fe1
Steve Wong <stevewong@Steves-MacBook-Pro.local>
2019-11-02 20:10:01 -0400

    second commit

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
-print("hello")
+class Users(object):
+    def __init__():
+        self.x = x
+        self.y = y
+        
+
+class Volunteers(Users):
+    
+    
+class Organizations(Volunteers):
+    
+class Individuals(Volunteers):
+    
+class Recipients(Users):
+    def __init__():
+        super().__init__()
+        self
+    
+    
+    
+    
+########
+
+class Goods(object):
